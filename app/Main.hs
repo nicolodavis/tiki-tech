932134322dc58a8155783afe169ce79479910170
Nicolo John Davis <nicolodavis@gmail.com>
2025-10-16 20:40:33 -0600

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -3,22 +3,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use fewer imports" #-}
 
 module Main where
 
-import Foreign.C.Types (CInt)
-import qualified MyLib (someFunc)
-import qualified SDL as S
-import qualified SDL.Font as SDLFont
-import qualified SDL.Framerate as SF
-import qualified SDL.Primitive as SP
-import qualified SDL.Video.Renderer as SVR
-import SDL.Vect             (V2(..), V3(..), V4(..), _x, _y, _z, Point(..))
-import SDL                  (($=))
 import Football.Player
-import Render
 import Football.Ball
-import Core
 import Control.Monad (void)
 import Control.Lens ((^.))
 
@@ -26,9 +17,7 @@ import Control.Monad.IO.Class
 import Control.Concurrent.STM (atomically, readTVar, writeTVar, newTVarIO, newEmptyTMVarIO)
 import Football.Match
 import Football.Match.Engine
-import Football.Types
 import Data.Foldable (traverse_)
-import Voronoi.JCVoronoi
 
 import qualified Data.Map as Map
 import Football.Understanding.Space.Data (SpaceMap(SpaceMap))
@@ -46,29 +35,9 @@ import Football.Understanding.Space (getSpaceMapForTeam, offsideLine)
 import Football.Understanding.LineBreaking (oppositionLines)
 import Football.Locate2D (Locate2D(locate2D))
 import Football.MatchStats (passesCompleted, oppositionPassesPerDefensiveAction, possession, shots, pitchTilt, interceptions, tackles, corners)
-import Football.Types (TeamId(TeamId1))
 import Core (Log(logOutput))
-
-black :: SP.Color
-black = V4 0 0 0 255
-
-white :: SP.Color
-white = V4 255 255 255 255
-
-red :: SP.Color
-red = V4 255 0 0 255
-
-orange :: SP.Color
-orange = V4 255 165 0 255
-
-blue :: SP.Color
-blue = V4 0 0 255 255
-
-purple :: SP.Color
-purple = V4 255 0 255 255
-
-grey :: SP.Color
-grey = V4 155 155 155 255
+import Football.Types
+import Linear (V3(..))
 
 -- Export the main function as hs_start in the Wasm module.
 foreign export javascript "hs_start"
@@ -84,23 +53,7 @@ renderFPS = 60
 main :: IO ()
 main = do
   trySomeBalls
-  S.initialize [S.InitVideo]
-  SDLFont.initialize
-  font <- SDLFont.load "Roboto-Regular.ttf" 18
-  h2font <- SDLFont.load "Roboto-Regular.ttf" 32
-  w <- S.createWindow "Tiki Tech" S.defaultWindow { S.windowInitialSize = V2 screenWidth screenHeight }
-  r <- S.createRenderer w (-1) S.defaultRenderer
-  S.showWindow w
-
-  let fonts = Fonts {fontsH2 = h2font, fontsDefault = font}
-  
-  SF.with renderFPS $ loopFor r fonts
-
-  S.destroyWindow w
-  S.quit
-  where
-    screenWidth = 2300
-    screenHeight = 1460
+  loopFor
 
 playerDefaultAcceleration :: Double
 playerDefaultAcceleration = 1.3
@@ -388,14 +341,9 @@ processLoop desiredFps = do
   liftIO $ threadDelay eDiff
   processLoop desiredFps
 
-data Fonts = Fonts
-  { fontsH2:: SDLFont.Font
-  , fontsDefault :: SDLFont.Font
-  }
 
-
-loopFor :: S.Renderer -> Fonts-> SF.Manager -> IO ()
-loopFor r fonts fpsm = do
+loopFor :: IO ()
+loopFor = do
   let playerStates = [player, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player1B, player2B, player3B, player4B, player5B, player6B, player7B, player8B, player9B, player10B, player11B]
   let playerMap = Map.fromList $ fmap (\p -> (playerStatePlayer p, p)) playerStates
   let liverbird = Team
@@ -450,93 +398,4 @@ loopFor r fonts fpsm = do
           , matchStateSpaceCache = spacecache
           , matchStateTeams = Map.fromList [(TeamId1,liverbird),(TeamId2,manshippy)]
           }
-  _ <- forkIO $ runAppM (processLoop processFps) initialState
-  runAppM loop' initialState
-  where
-    loop' :: AppM ()
-    loop' = do
-      frames <- fromIntegral `fmap` SF.count fpsm
-      -- Clear the screen!
-      S.rendererDrawColor r $= black
-      S.clear r
-
-      -- draw the pitch
-      pitch' <- pitch
-      liftIO $ render r (fontsDefault fonts) pitch'
-
-      -- draw the players
-      players <- allPlayers
-      traverse_ (liftIO . render r (fontsDefault fonts)) players
-
-      -- draw the ball
-      ball' <- gameBall
-      liftIO $ render r (fontsDefault fonts) ball'
-
-      -- draw space polygons
-      -- (SpaceMap sitesAll) <- spaceMap
-      -- traverse_ (liftIO . render r (fontsDefault fonts)) $ fmap snd $ Map.toList sitesAll
-
-
-      -- draw team space polygons
-      -- (SpaceMap t1Sites) <- getSpaceMapForTeam Team1
-      -- traverse_ (liftIO . render r (fontsDefault fonts)) $ fmap snd $ Map.toList t1Sites
-
-      -- (SpaceMap t2Sites) <- getSpaceMapForTeam Team2
-      -- traverse_ (liftIO . render r (fontsDefault fonts)) $ fmap snd $ Map.toList t2Sites
-
-      --draw zone polygons
-      -- (ZoneMap sitesAll) <- getZoneMap TeamId1
-      -- traverse_ (liftIO . render r (fontsDefault fonts)) $ fmap snd $ Map.toList sitesAll
-
-      -- offside1X <- offsideLine TeamId1
-      -- liftIO $ render r (fontsDefault fonts) (OffsideLine offside1X)
-      -- offside2X <- offsideLine TeamId2
-      -- liftIO $ render r (fontsDefault fonts) (OffsideLine offside2X)
-
-
-      -- lines <- oppositionLines Team1 
-      -- let lines' = fmap (\(l1, l2) -> DefLine l1 l2) lines
-      -- traverse_ (liftIO . render r (fontsDefault fonts)) lines'
-
-      -- draw the scores
-      (lg, mg) <- score
-
-      pos1 <- possession TeamId1
-      shots1 <- shots TeamId1
-      (pc1, pa1) <- passesCompleted TeamId1
-      pitchTilt1 <- pitchTilt TeamId1
-      ints1 <- interceptions TeamId1
-      tacks1 <- tackles TeamId1
-      oppppda1 <- oppositionPassesPerDefensiveAction TeamId1
-      corners1 <- corners TeamId1
-      pos2 <- possession TeamId2
-      shots2 <- shots TeamId2
-      (pc2, pa2) <- passesCompleted TeamId2
-      pitchTilt2 <- pitchTilt TeamId2
-      ints2 <- interceptions TeamId2
-      tacks2 <- tackles TeamId2
-      oppppda2 <- oppositionPassesPerDefensiveAction TeamId2
-      corners2 <- corners TeamId2
-
-      let board1 = StatsBoard TeamId1 pos1 shots1 pc1 pa1 pitchTilt1 ints1 tacks1 oppppda1 corners1
-      let board2 = StatsBoard TeamId2 pos2 shots2 pc2 pa2 pitchTilt2 ints2 tacks2 oppppda2 corners2
-      liftIO $ render r (fontsDefault fonts) board1
-      liftIO $ render r (fontsDefault fonts) board2
-
-      (GameTime _ time) <- currentGameTime
-      let (mm, ss) = (time `quot` 1000000) `quotRem` 60
-
-      surf <- SDLFont.solid (fontsH2 fonts) white (T.pack (TText.printf "%02d" mm) <> ":" <> T.pack (TText.printf "%02d" ss) <> " Liverbird " <> T.pack (show lg) <> " - " <> T.pack (show mg) <> " Man Shippy")
-      timeAndScoreTexture <- liftIO $ SVR.createTextureFromSurface r surf
-      surfDimensions <- liftIO $  SVR.surfaceDimensions surf
-      SVR.freeSurface surf
-      S.rendererDrawColor r $= black
-      let targetRect = S.Rectangle (P $ S.V2 150 20) surfDimensions
-      _ <- liftIO $ S.fillRect r (Just targetRect)
-      SVR.copy r timeAndScoreTexture Nothing (Just targetRect)
-      S.destroyTexture timeAndScoreTexture
-
-      S.present r
-      SF.delay_ fpsm
-      loop'
-
+  runAppM (processLoop processFps) initialState
